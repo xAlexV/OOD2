2258ababe1a26436b46ba4ec337c41a5cc97d1d3
Daria <padfone66@gmail.com>
2016-01-15 18:50:47 +0100

    trackform

---
+++ FlowNetwork/TrackBar.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace FlowNetwork
+{
+    partial class TrackBar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.btconfirm = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(30, 21);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 217);
+            this.trackBar1.TabIndex = 0;
+            // 
+            // btconfirm
+            // 
+            this.btconfirm.Location = new System.Drawing.Point(150, 82);
+            this.btconfirm.Name = "btconfirm";
+            this.btconfirm.Size = new System.Drawing.Size(75, 23);
+            this.btconfirm.TabIndex = 1;
+            this.btconfirm.Text = "confirm";
+            this.btconfirm.UseVisualStyleBackColor = true;
+            this.btconfirm.Click += new System.EventHandler(this.btconfirm_Click);
+            // 
+            // TrackBar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btconfirm);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "TrackBar";
+            this.Text = "TrackBar";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button btconfirm;
+    }
+}
---
+++ FlowNetwork/TrackBar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlowNetwork
+{
+    public partial class TrackBar : Form
+    {
+        public int trackflow;
+        public TrackBar()
+        {
+             
+            InitializeComponent();
+        }
+
+        private void btconfirm_Click(object sender, EventArgs e)
+        {
+            trackflow = trackBar1.Value;
+            this.Close();
+
+           
+        }
+
+
+    }
+}
